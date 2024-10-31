5a97fd50019e8da3aa564aa44392b9dfad0d043d
Ashley Natasha <ashleyotsiula@gmail.com>
2024-10-31 12:27:35 +0300

    events in react

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,11 @@
 // Code EyesOnMe Component Here
+export default function EyesOnMe(){
+    function focus(){console.log("Good!")}
+    function blur(){console.log("Hey! Eyes on me!");
+    }
+    return(
+        <div>
+            <button onFocus={focus} onBlur={blur}>Eyes on me</button>
+        </div>
+    )
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function onchange(){
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={onchange}></input>
+        </div>
     )
 }
 
